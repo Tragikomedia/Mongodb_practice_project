fd7c1d009c17daade7a3d181e41f7d34a8ef2f8f
Tragikomedia <kamilk9625@gmail.com>
2021-01-31 22:20:02 +0100

    Finished book-related functionalities.

--- routes/books.js
+++ routes/books.js
@@ -27,7 +27,7 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/new', async (req, res) => {
-    renderNewPage(res, new Book());
+    renderFormPage(res, new Book(), 'new');
 });
 
 router.post('/', async (req, res) => {
@@ -42,18 +42,77 @@ router.post('/', async (req, res) => {
 
     try {
         const newBook = await book.save();
-        res.redirect('books');
+        res.redirect(`books/${newBook.id}`);
     } catch {
         renderNewPage(res, book, true);
     }
 });
 
-async function renderNewPage(res, book, hasError = false) {
+router.get('/:id', async (req, res) => {
+    try {
+        // Populate is similar to join
+        const book = await Book.findById(req.params.id).populate('author').exec();
+        res.render('books/show', { book: book});
+    } catch {
+        res.redirect('/');
+    }
+});
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        renderFormPage(res, book, 'edit');
+    } catch {
+        res.redirect('/');
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    let book;
+
+    try {
+        book = await Book.findById(req.params.id);
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.publishDate = new Date(req.body.publishDate);
+        book.pageCount = req.body.pageCount;
+        book.description = req.body.description;
+        if (req.body.cover != null && req.body.cover !== '') {
+            saveCover(book, req.body.cover);
+        }
+        await book.save();
+        res.redirect(`/books/${book.id}`);
+    } catch {
+        if (book != null) {
+            renderFormPage(res, book, 'edit', true);
+        }
+        res.redirect('/');
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    let book;
+    try {
+        book = await Book.findById(req.params.id);
+        await book.remove();
+        res.redirect('/books');
+    } catch {
+        if (book != null) {
+            res.render(`books/${book.id}`, { book: book, errorMessage: 'Could not delete book'});
+        } else {
+            res.redirect('/');
+        }
+    }
+});
+
+async function renderFormPage(res, book, form, hasError = false) {
     try {
         const authors = await Author.find({});
         const params = {authors: authors, book: book};
-        if (hasError) params.errorMessage = "Error Creating Books";
-        res.render('books/new', params);
+        if (hasError) {
+            params.errorMessage = form === 'edit' ? 'Error Updating Book' : "Error Creating Books";
+        }
+        res.render(`books/${form}`, params);
     } catch {
         res.redirect('books');
     }
